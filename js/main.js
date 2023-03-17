96ffeb22e2700535ce5818908e8cbb34f4c15b2b
hesrun <vladmih1988@mail.ru>
2023-03-17 21:55:32 +0100

    corrects

--- js/main.js
+++ js/main.js
@@ -16,3 +16,26 @@ $(window).scroll(function(){
     }
 })
 
+$('.field').each(function() {
+    var field = $(this);
+    var input = field.find('.field__input');
+    function checkInput() {
+        var valueLength = input.val().length;
+        console.log(valueLength);
+        if (valueLength > 0 ) {
+            field.addClass('field_active')
+        } else {
+            field.removeClass('field_active')
+        }
+    }
+    $('.field__input').blur(function() {
+        checkInput();
+        $(this).parents('.field').removeClass('field_focus');
+    })
+    $('.field__input').focus(function() {
+        checkInput();
+        $(this).parents('.field').addClass('field_focus');
+    })       
+});
+
+
